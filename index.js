023716a6376c4f457ff15096847e35bd6e3c9b71
Jeff <jeff@tpcinternet.com>
2023-09-25 12:28:45 +0900

    feat: code for vue3

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 import anime from 'animejs/lib/anime.es.js'
+import { isVue2 } from 'vue-demi'
 
-function install(Vue) {
-  Vue.directive('anime', {
-    bind: function bind(targets, binding) {
-      var opts = Object.assign({}, binding.value, { targets: targets });
-      anime(opts);
-    },
-    update: function bind(targets, binding) {
-      var opts = Object.assign({}, binding.value, { targets: targets });
-      anime(opts);
-    }
-  });
-  Vue.prototype.$anime = anime;
+let install = () => {}
+
+if (isVue2) {
+  install = function (Vue) {
+    Vue.directive('anime', {
+      bind: function bind(targets, binding) {
+        var opts = Object.assign({}, binding.value, { targets: targets });
+        anime(opts);
+      },
+      update: function bind(targets, binding) {
+        var opts = Object.assign({}, binding.value, { targets: targets });
+        anime(opts);
+      }
+    });
+    Vue.prototype.$anime = anime;
+  }
+} else {
+  install = function (app, options) {
+    app.directive('anime', {
+      bind: function bind(targets, binding) {
+        var opts = Object.assign({}, binding.value, { targets: targets });
+        anime(opts);
+      },
+      update: function bind(targets, binding) {
+        var opts = Object.assign({}, binding.value, { targets: targets });
+        anime(opts);
+      }
+    });
+    app.prototype.$anime = anime;
+  }
 }
 
 export default {
